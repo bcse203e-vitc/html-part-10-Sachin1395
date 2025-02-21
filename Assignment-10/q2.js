d2c068cd413392f38d9734a49c261d485e22dd13
Sachin1395 <sachin13905@gmail.com>
2025-02-21 13:22:23 +0530

    Add files via upload

---
+++ Assignment-10/q2.js
@@ -0,0 +1,7 @@
+document.getElementById('redButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blueButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
